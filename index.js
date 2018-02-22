11c52217f8faa076e451e807e3b9b5056a93365f
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-02-21 19:48:04 -0800

    Refactor to native readline

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 const { Game } = require('./tictactoe.js');
-const program = require('commander');
+const readline = require('readline');
 
-program.version('0.0.1').description('Tic-Tac-Toe for the modern command line');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-let currentGame = null;
+let currentGame;
 
-program
-  .command('start')
-  .alias('s')
-  .description('Starts a new game')
-  .action(() => {
+const newGame = () => {
+  console.log('Hit "enter" to start a new game');
+  currentGame = null;
+};
+
+newGame();
+
+const playAgain = () => {
+  rl.question('Play Again? Y/N ', (answer) => {
+    if (answer === 'Y') {
+      newGame();
+    } else if (answer === 'N') {
+      console.log('Sorry to see you go');
+      // somehow exit the process
+      rl.close();
+    } else {
+      console.log(`Where I come from, ${answer} means yes`);
+      newGame();
+    }
+  });
+};
+
+const gameOn = () => {
+  rl.question(
+    'Which square would you like? Select your square, then hit "enter" ',
+    (square) => {
+      currentGame.move(square);
+      let winningMessage = currentGame.isWinner();
+      if (winningMessage === null) {
+        gameOn();
+      } else {
+        console.log(winningMessage);
+        playAgain();
+      }
+    },
+  );
+};
+
+rl.on('line', () => {
+  if (currentGame === null) {
     currentGame = new Game();
-  });
-
-program
-  .command('move <square>')
-  .description('Makes a move on the board')
-  .action((square) => {
-    currentGame.move(square);
-  });
-
-program.parse(process.argv);
+    currentGame.display();
+    gameOn();
+  }
+});
